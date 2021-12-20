5f88c4705c25e00b8c5fc842ca54074509bc6e59
Steve Kinney <hello@stevekinney.net>
2021-12-20 14:35:59 -0600

    Change to pink

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 export const Navigation = ({ children }) => {
   return (
-    <nav className="col-span-3 flex flex-col border-r-2 border-indigo-400 h-full">
+    <nav className="col-span-3 flex flex-col border-r-2 border-pink-400 h-full">
       {children}
     </nav>
   );
@@ -14,8 +14,8 @@ export const NavigationLink = ({ children, to }) => {
       to={to}
       className={({ isActive }) =>
         isActive
-          ? 'bg-indigo-300 hover:bg-indigo-400'
-          : 'bg-white hover:bg-indigo-100'
+          ? 'bg-pink-300 hover:bg-pink-400'
+          : 'bg-white hover:bg-pink-100'
       }
     >
       <article className="w-full p-4">{children}</article>
